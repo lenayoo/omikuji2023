8fb31428e14e26c12bd6f932304aed8d9cd2cca3
Lena Yoo <lenayoo17@gmail.com>
2023-06-20 22:26:37 +0900

    add random function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
+import './index.css'
+import './assets/fortunecookie'
+import { fortuneCookie } from './assets/fortunecookie'
+import { useState } from 'react'
 
 function App() {
+  const [cookie, setCookie] = useState('')
+
+  function clickHandler() {
+    let randomIndex = Math.floor(Math.random() * fortuneCookie.length)
+    let randomCookie = fortuneCookie[randomIndex]
+    setCookie(randomCookie)
+  }
+
   return (
     <>
       <h1>Find your luck today!</h1>
-      <button>Click to see today's luck</button>
-      <div>
-        <p>Today's your luck is...</p>
+      <button className="open-omikuji" onClick={clickHandler}>
+        Click to see today's luck
+      </button>
+      <div className="luckybox">
+        <h3 className="text-white">Today's your luck is...</h3>
         <div className="line"></div>
-        <p>very very good</p>
+        <p key={Math.random()}>{cookie}</p>
       </div>
     </>
   )
